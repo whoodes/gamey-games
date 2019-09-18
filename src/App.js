f4fa5bdda5abacb10c3fec8b28a89473d4e75808
Wyatt Hoodes <hoodesw@hawaii.edu>
2019-09-17 21:44:57 -1000

    App: Replace logo with title.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
 import "./App.css"
 
 class LambdaDemo extends Component {
@@ -36,10 +35,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h1>Gamey Games</h1>
           <LambdaDemo />
         </header>
       </div>
